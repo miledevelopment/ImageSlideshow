87821289a21ef081bf812e3794ed89f6202e05f9
Ivan Djordjevic <subaru300hp@live.com>
2026-03-10 14:51:57 +0100

    Dependencies update

--- Package.swift
+++ Package.swift
@@ -12,9 +12,24 @@ let package = Package(
             targets: ["ImageSlideshow"]
         )
     ],
+    dependencies: [
+        // external libraries
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.2.0"),
+        .package(url: "https://github.com/AFNetworking/AFNetworking.git", from: "4.0.0"),
+        .package(url: "https://github.com/rs/SDWebImage.git", from: "5.0.0"),
+        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
+        .package(url: "https://github.com/ParsePlatform/Parse-SDK-iOS-OSX.git", from: "1.19.0")
+    ],
     targets: [
         .target(
             name: "ImageSlideshow",
+            dependencies: [
+                "AlamofireImage",
+                "AFNetworking",
+                "SDWebImage",
+                "Kingfisher",
+                "Parse"
+            ],
             path: "ImageSlideshow"
         )
     ]
