1d69117c53f59791464c0484379e03aa4dbaf26f
Ivan Djordjevic <subaru300hp@live.com>
2026-03-10 16:36:02 +0100

    Remove packages folder re-rout to original

--- Package.swift
+++ Package.swift
@@ -8,30 +8,17 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "MyAppImageSlideshow",
-            targets: ["MyAppImageSlideshow"]
+            name: "ImageSlideshow",
+            targets: ["ImageSlideshow"]
         )
-    ],
-    dependencies: [
-        // external libraries
-//        .package(url: "https://github.com/zvonicek/ImageSlideshow.git", from: "1.8.1"),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", from: "4.2.0"),
-        .package(url: "https://github.com/AFNetworking/AFNetworking.git", from: "4.0.0"),
-        .package(url: "https://github.com/rs/SDWebImage.git", from: "5.0.0"),
-        .package(url: "https://github.com/onevcat/Kingfisher.git", from: "7.0.0"),
-        .package(url: "https://github.com/ParsePlatform/Parse-SDK-iOS-OSX.git", from: "1.19.0")
     ],
     targets: [
         .target(
-            name: "MyAppImageSlideshow",
-            dependencies: [
-                "AlamofireImage",
-                "AFNetworking",
-                "SDWebImage",
-                "Kingfisher",
-                "Parse"
-            ],
-            path: "ImageSlideshow"
+            name: "ImageSlideshow",
+            path: "ImageSlideshow/Classes",
+            resources: [
+                .process("../Assets")
+            ]
         )
     ]
 )
