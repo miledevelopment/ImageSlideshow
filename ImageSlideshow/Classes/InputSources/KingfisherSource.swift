46f0eb2c99c43f216c37f26e9e02386bfefede1c
Ivan Djordjevic <subaru300hp@live.com>
2026-03-11 10:28:41 +0100

    Kingfisher deprecated methode

--- ImageSlideshow/Classes/InputSources/KingfisherSource.swift
+++ ImageSlideshow/Classes/InputSources/KingfisherSource.swift
@@ -5,26 +5,23 @@
 //  Created by feiin
 //
 //
-
 import Foundation
 import UIKit
 import Kingfisher
 
 /// Input Source to image using Kingfisher
+@MainActor
 public class KingfisherSource: NSObject, InputSource {
-    /// url to load
+    /// URL to load
     public var url: URL
 
-    /// placeholder used before image is loaded
+    /// Placeholder used before image is loaded
     public var placeholder: UIImage?
     
-    /// options for displaying, ie. [.transition(.fade(0.2))]
+    /// Options for displaying, e.g. [.transition(.fade(0.2))]
     public var options: KingfisherOptionsInfo?
 
     /// Initializes a new source with a URL
-    /// - parameter url: a url to be loaded
-    /// - parameter placeholder: a placeholder used before image is loaded
-    /// - parameter options: options for displaying
     public init(url: URL, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
         self.url = url
         self.placeholder = placeholder
@@ -33,28 +30,25 @@ public class KingfisherSource: NSObject, InputSource {
     }
 
     /// Initializes a new source with a URL string
-    /// - parameter urlString: a string url to load
-    /// - parameter placeholder: a placeholder used before image is loaded
-    /// - parameter options: options for displaying
     public init?(urlString: String, placeholder: UIImage? = nil, options: KingfisherOptionsInfo? = nil) {
-        if let validUrl = URL(string: urlString) {
-            self.url = validUrl
-            self.placeholder = placeholder
-            self.options = options
-            super.init()
-        } else {
-            return nil
-        }
+        guard let validUrl = URL(string: urlString) else { return nil }
+        self.url = validUrl
+        self.placeholder = placeholder
+        self.options = options
+        super.init()
     }
     
-    /// Load an image to an UIImageView
-    ///
+    /// Load an image to a UIImageView
     /// - Parameters:
     ///   - imageView: UIImageView that receives the loaded image
     ///   - callback: Completion callback with an optional image
-    @objc
     public func load(to imageView: UIImageView, with callback: @escaping (UIImage?) -> Void) {
-        imageView.kf.setImage(with: self.url, placeholder: self.placeholder, options: self.options, progressBlock: nil) { result in
+        imageView.kf.setImage(
+            with: self.url,
+            placeholder: self.placeholder,
+            options: self.options,
+            progressBlock: nil
+        ) { result in
             switch result {
             case .success(let image):
                 callback(image.image)
@@ -65,8 +59,7 @@ public class KingfisherSource: NSObject, InputSource {
     }
     
     /// Cancel an image download task
-    ///
-    /// - Parameter imageView: UIImage view with the download task that should be canceled
+    /// - Parameter imageView: UIImageView with the download task that should be canceled
     public func cancelLoad(on imageView: UIImageView) {
         imageView.kf.cancelDownloadTask()
     }
